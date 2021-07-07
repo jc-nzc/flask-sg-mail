bab861e60da391078f797c5171a9ce290d167d6a
Zero <node.zerocool@gmail.com>
2021-07-07 01:02:28 -0500

    app

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'apikey'
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+mail = Mail(app)
